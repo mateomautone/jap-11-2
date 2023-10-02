721e055fecc3bb235d1ba9028077232276ca76eb
Mateo <mateomautone98@gmail.com>
2023-10-01 22:17:59 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+function enviarDatos(){
+    let fname = document.getElementById('fname').value
+    let lname = document.getElementById('lname').value
+    let fnac = document.getElementById('fnac').value
+    jsonAEnviar = JSON.stringify({ nombre: fname, apellido: lname, fechanacimientl: fnac})
+    // console.log(jsonAEnviar)
+    const options = {
+        method: 'POST',
+        headers: {
+        'Content-Type': 'application/json',
+        },
+        body: jsonAEnviar,
+    };
+    fetch('https://jsonplaceholder.typicode.com/users', options)
+        .then(coso => console.log(coso))
+}
+
+document.getElementById('enviar').addEventListener("click", enviarDatos)
